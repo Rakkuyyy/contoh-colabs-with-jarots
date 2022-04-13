582e300097ee78d9a87fefee80446c0db9fe0510
dulziz00@gmail.com <aziz@LAPTOP-DDK7PHA8.localdomain>
2022-04-13 16:46:25 +0700

    Add some file

---
+++ javascript-dasar.js
@@ -0,0 +1,39 @@
+//  Array
+let iniArray = [
+    'dandun',
+    'gigih',
+    17,
+    ['Jarot','Nopal','Doni']
+];
+
+//  Object
+let manusia = {
+    firstName : 'Dandun',
+    lastname : 'Gigih',
+    age : 2001 - 2020,
+
+}  
+
+// perulangan
+// for
+for (let ulang = 1; ulang <= 10; ulang++) {
+    console.log(`Ulangi ${ulang}`);
+}
+
+//While
+var i = prompt("Masukkan jumlah :" ,10);
+while(i>0){
+    document.write("Hello");
+    i--;
+}
+
+//Do While
+jumlah = 1
+ do {
+  console.log('Hello World!'+jumlah)
+  jumlah++
+ }while(jumlah<=10)
+
+
+console.log('test');
+console.log('test');
